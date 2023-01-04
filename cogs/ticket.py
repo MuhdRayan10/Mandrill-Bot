5fc525b9f6817bf0452ae5c3e75dc742d4d53926
muhdrayan <muhammedrayansavad1@gmail.com>
2023-01-04 13:04:16 +0300

    Perfected ticket.py

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -3,17 +3,21 @@ from discord.ui import View
 from discord import app_commands
 import discord
 
+from helpers import Var
+
+# Ticket settings Interface buttons
 class TicketSettings(View):
     def __init__(self):
         super().__init__(timeout=None)
 
+    # To close the ticket and delete channel
     @discord.ui.button(label="Close Ticket", style=discord.ButtonStyle.red, emoji="🔒")
     async def close(self, interaction, _):
         await interaction.response.send_message("Ticket is being closed...", ephemeral=True)
         await interaction.channel.delete()
 
 
-
+# Ticket Creation Interface buttons
 class CreateTicket(View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -24,8 +28,9 @@ class CreateTicket(View):
         await interaction.response.defer()
 
         msg = await interaction.followup.send(content="A ticket is being created!", ephemeral=True)
-        other_role = interaction.guild.get_role(102939012093) #mods
+        other_role = interaction.guild.get_role(Var.guardrill_role) # Mod Role
 
+        # What permissions to keep
         overwrites = {
             interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
             interaction.guild.me: discord.PermissionOverwrite(read_messages=True),
@@ -33,6 +38,7 @@ class CreateTicket(View):
             #other_role: discord.PermissionOverwrite(read_messages=True)
         }
 
+        # Get Tickets category and create a new channel
         category = discord.utils.get(interaction.guild.categories, name='Tickets')
         channel = await interaction.guild.create_text_channel(f"[TICKET]-{interaction.user.name}",
                 overwrites=overwrites, category=category)
@@ -42,15 +48,20 @@ class CreateTicket(View):
         embed = discord.Embed(title="Ticket Created!", description=f"{interaction.user.mention} created a ticket! \nTo close the ticket, click the button below.")
         await channel.send(embed=embed, view=TicketSettings())
 
+# Cog class
 class Tickets(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Setup Ticket Interface in given text channel
+    @app_commands.checks.has_permissions(manage_guild=True)
     @app_commands.command(name="setup-tickets", description="[MODS] Setup Ticket Interface")
-    async def setup_tickets(self, interaction):
+    @app_commands.describe(channel="The channel where the Ticket Interface is to be set up")
+    async def setup_tickets(self, interaction, channel: discord.TextChannel):
+        
         embed = discord.Embed(title="Create a Ticket!", description="Click on the `Create Ticket` button below to create a ticket. The server's staff will be notified and shortly aid you with your problem.")
-        await interaction.response.send_message(embed=embed, view=CreateTicket())
+        await channel.send(embed=embed, view=CreateTicket())
 
-
+# Cog setup function
 async def setup(bot):
     await bot.add_cog(Tickets(bot))
