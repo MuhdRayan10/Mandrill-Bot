54ed6292eb41bb5a6c84bbe6506362e7dfc9ed47
muhdrayan <muhammedrayansavad1@gmail.com>
2023-01-05 22:38:40 +0300

    XP Update

--- cogs/xp.py
+++ cogs/xp.py
@@ -3,6 +3,10 @@ from discord import app_commands
 from easy_sqlite3 import *
 import random
 import discord
+from easy_pil import Editor, Font, load_image_async, Canvas
+
+from helpers import Var as V
+Var = V()
 
 class XP(commands.Cog):
     def __init__(self, bot):
@@ -22,21 +26,22 @@ class XP(commands.Cog):
         db = Database("./data/levels")
 
         if not db.if_exists("levels", where={"user":auth_id}):
-            db.insert("levels", (auth_id, 0, 0, 100))
+            db.insert("levels", (auth_id, 0, 0, 100, 0))
 
         levels = db.select("levels", where={"user":auth_id}, size=1)
-        addition = 5 + random.choice([-1, 0, 1])
+        xp = 5 + random.choice([-1, 0, 1])
 
-        if levels[2] >= levels[-1]:
-
-            db.update("levels", {"level":levels[1]+1, "xp":levels[2] + addition - levels[1], "lim":levels[3] * 1.5, "total": levels[4]+addition},
-                 where={"user":auth_id})
+        if levels[2] + xp >= levels[3]:
+            db.update("levels", {"level":levels[1]+1, "xp":levels[2]+xp-levels[3], "lim":int(1.5*levels[3]), "total":levels[4]+xp},
+                where={"user":auth_id})
+            print(levels[2]+xp-levels[3])
 
         else:
-            db.update("levels", information={"xp":levels[2]+addition, "total":levels[4]+addition}, where={"user":auth_id})
+            db.update("levels", {"xp":levels[2]+xp, "total":levels[4]+xp}, where={"user":auth_id})
 
-        print(f"[LOG] - {message.author.name} - {levels[4]} XP")
+        print(f"[LOG] - {message.author.name} - Lvl. {levels[1]} - {levels[2]+xp} XP")
         db.close()
+
 
     @app_commands.command(name="level", description="View user's level in the server")
     @app_commands.describe(user="The user to be viewed")
@@ -46,12 +51,32 @@ class XP(commands.Cog):
         db = Database("./data/levels")
         
         if not db.if_exists("levels", {"user":user.id}):
-            db.insert("levels", (user.id, 0, 0, 100))
+            db.insert("levels", (user.id, 0, 0, 100, 0))
 
-        data = db.select("levels", where={"user":user.id})
+        data = db.select("levels", where={"user":user.id}, dict_format=True)
+        db.close()
 
-        embed = discord.Embed(title=user.name, description=f"XP: {data[2]} \Level: {data[1]}")
-        await interaction.response.send_message(embed=embed)
+        bg = Editor(Canvas((900, 300), color="#141414"))
+        profile_picture = await load_image_async(str(user.avatar.url))
+        profile = Editor(profile_picture).resize((150, 150)).circle_image()
+
+        poppins = Font.poppins(size=40)
+        poppins_small = Font.poppins(size=30)
+
+        card_shape = [(600, 0), (750, 300), (900, 300), (900, 0)]
+        bg.polygon(card_shape, color="#FFFFFF")
+        bg.paste(profile, (30, 30))
+
+        bg.rectangle((30, 220), width=650, height=40, color="#FFFFFF")
+        bg.bar((30, 220), max_width=650, height=40, percentage=data["xp"]/data["lim"], color="#FFFFFF", radius=20)
+        bg.text((200, 40), user.display_name, font=poppins, color="#FFFFFF")
+
+        bg.rectangle((200, 100), width=350, height=2, fill="#FFFFFF")
+        bg.text((200, 130), f"Level - {data['level']} | XP - {data['xp']}/{data['lim']}", font=poppins_small, color="#FFFFFF")
+
+        file = discord.File(fp=bg.image_bytes, filename=f"{user.name}_level.png")
+        await interaction.response.send_message(file=file)
+
 
 
 
