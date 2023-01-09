f1643d03fd43472e349656d397f110f54f29bcb8
muhdrayan <muhammedrayansavad1@gmail.com>
2023-01-09 14:26:51 +0300

    final version

--- functions.py
+++ functions.py
@@ -4,31 +4,36 @@ import discord
 async def update_criteria(inviter, guild, V):
     
     db2 = Database("./data/invites")
-    data = db2.select("invites", where={"inviter":inviter.id}, size=1)
+    data = db2.select("invites", where={"inviter":inviter.id})
 
     db2.close()
 
+    count = 0
+    for d in data:
+        count += d[2]
+
     db = Database("./data/criteria")
-    if not db.if_exists():
+    if not db.if_exists("role", where={"user":inviter.id}):
         db.insert("role", (inviter.id, 0, 0, 0, 0))
 
-    db.update("role", {"a1": data[1]}, where={"user":inviter.id})
-
+    db.update("role", {"a1": count}, where={"user":inviter.id})
     data = db.select("role", where={"user":inviter.id}, size=1)
+    db.close()
+
     if data[1] >= 2 and data[2] == 1 and not data[3]:
         channel = guild.get_channel(V.rendrill_channel)
         await inviter.send(f"Looks like you are almost eligible for the `Rendrill` role! To complete the quiz, go to {channel.mention} and click on the `GET RENDRILL` button to start the quiz!")
-    db.close()
+    
 
 
 def find_invite(li, code):
     for inv in li:
-        if inv.code == code:
+        if str(inv.code) == str(code):
             return inv
 
 async def update_invites(member, V):
     db = Database("./data/invites")
-    db.create_table("invites", {"inviter": INT, "code": INT, "uses": INT})
+    db.create_table("invites", {"inviter": INT, "code": STR, "uses": INT})
 
     invites = await member.guild.invites()
     old_invites = db.select("invites")
@@ -40,14 +45,12 @@ async def update_invites(member, V):
     for old_invite in old_invites:
         new_invite = find_invite(invites, old_invite[1])
         if old_invite[2] < new_invite.uses:
-            embed = discord.Embed(title="Invite", description="Information about the invite")
+            embed = discord.Embed(title="Invite", description="Information about the invite", color=V.base_color)
             embed.add_field(name="Inviter", value=new_invite.inviter.name)
-            embed.add_field(name="Uses", value=f"{new_invite.uses}")
+            embed.add_field(name="Uses", value=f"`{new_invite.uses}`")
             embed.add_field(name="Invited", value=member.name)
 
             inviter_.append(new_invite.inviter)
-
-            print("Log message")
 
             channel = member.guild.get_channel(V.log_channel)
             await channel.send(embed=embed)
@@ -55,7 +58,20 @@ async def update_invites(member, V):
     db = Database("./data/invites")
     db.clear_table('invites')
 
+    codes = [o[1] for o in old_invites]
+
     for invite in invites:
+        if invite.code not in codes:
+            embed = discord.Embed(title="Invite", description="Information about the invite", color=V.base_color)
+            embed.add_field(name="Inviter", value=invite.inviter.name)
+            embed.add_field(name="Uses", value=f"`{invite.uses}`")
+            embed.add_field(name="Invited", value=member.name)
+
+            inviter_.append(invite.inviter)
+
+            channel = member.guild.get_channel(V.log_channel)
+            await channel.send(embed=embed)
+
         db.insert("invites", (invite.inviter.id, invite.code, invite.uses))
 
     db.close()
