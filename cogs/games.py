ac0605a3dab3a8f45730cef36f227c1ad858813f
muhdrayan <muhammedrayansavad1@gmail.com>
2023-02-06 23:38:39 +0300

    fixed db

--- cogs/games.py
+++ cogs/games.py
@@ -37,7 +37,7 @@ class Games(commands.Cog):
 
         self.views = ui.View(timeout=None)
 
-        for i in range(8):
+        for i in range(10):
             spin_wheel = ui.Button(label="?", style=discord.ButtonStyle.blurple, custom_id=f"??{i}:blurple")
             spin_wheel.callback = self.spin_wheel
             self.views.add_item(spin_wheel)
@@ -46,8 +46,7 @@ class Games(commands.Cog):
     @app_commands.checks.has_any_role(Var.guardrill_role, Var.liberator_role)
     async def setup_mystery_box(self, interaction, channel:discord.TextChannel):
         embed = discord.Embed(title='Choose the Box Carefully...', color=Var.base_color)
-        embed.add_field(name="Prizes", value='- 1,111 $LEF - Native coin of the "Wild Network"\n- Mineral\n- NFT Comics Series "Chronicles of the Ten unique Flowers"\n- "Wild Network" Branded Merch & Physical Artwork (First Edition)',
-            color=Var.base_color)
+        embed.add_field(name="Prizes", value='- 1,111 $LEF - Native coin of the "Wild Network"\n- Mineral\n- NFT Comics Series "Chronicles of the Ten unique Flowers"\n- "Wild Network" Branded Merch & Physical Artwork (First Edition)')
         
         await channel.send(embed=embed, view=self.views)
         await interaction.response.send_message(f"Added `Mystery Box` interface, to <#{channel.id}>")
@@ -57,7 +56,7 @@ class Games(commands.Cog):
         if user.get_role(Var.rendrill_role) is None:
             embed = discord.Embed(
                 title="Not Eligible",
-                description=f"You will be eligible to open the Mystery Box after you the <@{Var.rendrill_role}> from <#{Var.rendrill_channel}>.",
+                description=f"You will be eligible to open the Mystery Box after you <#{Var.rendrill_channel}>.",
                 color=Var.base_color
             )
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -77,8 +76,7 @@ class Games(commands.Cog):
                 desc = f"You have already interacted in the past 4 days. Please try again in {time_left_in_days} days and {time_left_in_hours} hours."
                 embed = discord.Embed(
                     title="Spin the Wheel Prize",
-                    description=desc,
-                    color=Var.base_color
+                    description=desc, color=Var.base_color
                 )
                 
                 await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -91,7 +89,7 @@ class Games(commands.Cog):
         else:
             desc = f"**Congratulations!**\nYou have won the `{prize}`."
             db = Database("./data/prizes")
-            db.create_table("prizes", {"winner":INT, "name":"TEXT", "prize":STR})
+            db.create_table("prizes", {"winner":INT, "name":"TEXT", "prize":"TEXT"})
 
             db.insert("prizes", (interaction.user.id, interaction.user.name, prize))
             db.close()
